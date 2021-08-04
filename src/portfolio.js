87de4657ef25cb3dc996c0d53fea78fee6e01680
Javaid Ahmad <rahijavaid5@gmail.com>
2021-08-04 12:33:15 +0530

    updates

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,29 +1,30 @@
 import emoji from "react-easy-emoji";
 
-import googlelogo from "./assets/img/icons/common/google.svg";
-import github from "./assets/img/icons/common/github.svg";
-import airbnb from "./assets/img/icons/common/airbnbLogo.png";
+import infostride from "./assets/img/icons/common/infostride.jpg";
+import venturepact from "./assets/img/icons/common/venturepact.jpg";
+import calance from "./assets/img/icons/common/calance.png";
+import paxcom from "./assets/img/icons/common/paxcom.jpg";
 
 export const greetings = {
-	name: "Hanzla Tauqeer",
-	title: "Hi all, I'm Hanzla",
+	name: "Javaid Ahmad Ganaie",
+	title: "Hi all, I'm Javaid",
 	description:
-		"A passionate Full Stack Web Developer and Mobile App Developer having an experience of building Web applications with JavaScript / Reactjs / Nodejs / Python / Django / Flask and some other cool libraries and frameworks and Cross Platform Mobile Apps With Flutter.",
-	resumeLink: "https://cv.hanzla.ga",
+		"A passionate Full Stack Web Developer having an experience of building Web applications with JavaScript / Reactjs / Nodejs / Angular and some other cool libraries and frameworks and Cross Platform Mobile Apps With React native.",
+	resumeLink: "https://docs.google.com/document/d/1jKnO6dsNB4FhO6-0fCmNaLrHohvOva6tYkn7Wp0e8Kw/edit?usp=sharing",
 };
 
 export const openSource = {
-	githubUserName: "1hanzla100",
+	githubUserName: "JavaidAhmad",
 };
 
 export const contact = {};
 
 export const socialLinks = {
-	facebook: "https://www.facebook.com/1hanzla100",
-	instagram: "https://www.instagram.com/1hanzla100",
-	twitter: "https://twitter.com/1hanzla100",
-	github: "https://github.com/1hanzla100",
-	linkedin: "https://www.linkedin.com/in/hanzla-tauqeer-0869281ba/",
+	facebook: "https://www.facebook.com/rahi.javaid.44",
+	instagram: "https://www.instagram.com/javaid_maqbool/",
+	twitter: "https://twitter.com/RahiJavaid4",
+	github: "https://github.com/JavaidAhmad",
+	linkedin: "https://www.linkedin.com/in/javaid-ganaie-39ab42104/",
 };
 
 export const skillsSection = {
@@ -32,7 +33,7 @@ export const skillsSection = {
 		"CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
 	skills: [
 		emoji(
-			"⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications"
+			"⚡ Develop highly interactive Front end / User Interfaces for your web applications"
 		),
 		emoji(
 			"⚡ Progressive Web Applications ( PWA ) in normal and SPA Stacks"
@@ -72,22 +73,6 @@ export const skillsSection = {
 			fontAwesomeClassname: "logos:nodejs-icon",
 		},
 		{
-			skillName: "flutter",
-			fontAwesomeClassname: "logos:flutter",
-		},
-		{
-			skillName: "swift",
-			fontAwesomeClassname: "vscode-icons:file-type-swift",
-		},
-		{
-			skillName: "npm",
-			fontAwesomeClassname: "vscode-icons:file-type-npm",
-		},
-		{
-			skillName: "sql-database",
-			fontAwesomeClassname: "vscode-icons:file-type-sql",
-		},
-		{
 			skillName: "mongoDB",
 			fontAwesomeClassname: "vscode-icons:file-type-mongo",
 		},
@@ -100,16 +85,30 @@ export const skillsSection = {
 			fontAwesomeClassname: "logos:firebase",
 		},
 		{
-			skillName: "python",
-			fontAwesomeClassname: "logos:python",
-		},
-		{
 			skillName: "git",
 			fontAwesomeClassname: "logos:git-icon",
 		},
 		{
 			skillName: "docker",
 			fontAwesomeClassname: "logos:docker-icon",
+		},
+		
+		{
+			skillName: "kubernetes",
+			fontAwesomeClassname: "logos:cloud",
+		},
+
+		{
+			skillName: "terraform",
+			fontAwesomeClassname: "logos:dev",
+		},
+		{
+			skillName: "kafka",
+			fontAwesomeClassname: "logos:dev",
+		},
+		{
+			skillName: "microservices",
+			fontAwesomeClassname: "logos:dev",
 		},
 	],
 };
@@ -121,101 +120,99 @@ export const SkillBars = [
 	},
 	{
 		Stack: "Backend",
-		progressPercentage: "70",
+		progressPercentage: "90",
 	},
 	{
 		Stack: "Programming",
-		progressPercentage: "60",
+		progressPercentage: "85",
 	},
 ];
 
 export const educationInfo = [
 	{
-		schoolName: "Harvard University",
-		subHeader: "Master of Science in Computer Science",
-		duration: "September 2017 - April 2019",
-		desc: "Participated in the research of XXX and published 3 papers.",
+		schoolName: "Lovely Professional University",
+		subHeader: "Master of Computer Applications(MCA)",
+		duration: "2016-2018",
+		desc: "Participated in the multiple workshops.",
 		descBullets: [
-			"Lorem ipsum dolor sit amet, consectetur adipdfgiscing elit",
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
 		],
 	},
 	{
-		schoolName: "Harvard",
-		subHeader: "Master of Science in Computer Science",
-		duration: "September 2017 - April 2019",
-		desc: "Participated in the research of XXX and published 3 papers.",
+		schoolName: "Subharti Institute of Technology and Engineering",
+		subHeader: "Bachelor of Computer Applications",
+		duration: "2012-2015",
+		desc: "Participated in the multiple software engineering related workshops.",
 		descBullets: [
-			"Lorem ipsum dolor sit amet, consectetfgur adipiscing elit",
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
 		],
-	},
-	{
-		schoolName: "Stanford University",
-		subHeader: "Bachelor of Science in Computer Science",
-		duration: "September 2013 - April 2017",
-		desc: "Ranked top 10% in the program. Took courses about Software Engineering, Web Security, Operating Systems, ...",
-		descBullets: [
-			"Lorem ipsum dolorfdg sit amet, consectetur adipiscing elit",
-		],
-	},
+	}
 ];
 
 export const experience = [
 	{
-		role: "Software Engineer",
-		company: "Google",
-		companylogo: googlelogo,
-		date: "June 2018 – Present",
-		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+		role: "Senior Full Stack Developer",
+		company: "Paxcom (A Paymentus Company)",
+		companylogo: paxcom,
+		date: "Dec 2020 – Present",
+		desc: "Responsible to create and add new feature to the existing product Paymentus.",
 		descBullets: [
-			"Lorem ipsum dolor sit amet, consdfgectetur adipiscing elit",
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+			"Working on both frontend and backend in React and node.",
+			"Reviewed code, debugged problems and corrected issues.",
 		],
 	},
 	{
-		role: "Front-End Developer",
-		company: "Github",
-		companylogo: github,
-		date: "May 2017 – May 2018",
-		desc: "Lorem ipsum dolor sit amet, consefdctetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+		role: "Full Stack Developer",
+		company: "Infostride",
+		companylogo: infostride,
+		date: "June-2019 to December-2020",
+		desc: "Worked on Nomis Deal Manager projects using ReactJs, Meteor, MongoDB, Express, Node JS, Typescript.",
 	},
 	{
-		role: "Software Engineer Intern",
-		company: "Airbnb",
-		companylogo: airbnb,
-		date: "Jan 2015 – Sep 2015",
-		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+		role: "Full Stack Developer",
+		company: "Venturepact",
+		companylogo: venturepact,
+		date: "January-2018 to June-2019",
+		desc: "Developed two scalable Single Page web applications and two chat application using MEAN stack technologies. https://neta.co.in/,  http://f.rely.co/.",
+	},
+	{
+		role: "Consultant Technology",
+		company: "Calance Software Pvt. Ltd",
+		companylogo: calance,
+		date: "Aug 2015 – Aug 2016",
+		desc: "Used various technologies, including Html, CSS, Java script, PHP, Word press, and Java, to make repairs and optimize the corporate website.",
 	},
 ];
 
 export const projects = [
 	{
-		name: "lorem ipsum",
-		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
-		link: "https://example.com",
+		name: "Neta",
+		desc: "To increase transparency in Indian Politics. Neta is a cross party platform which is aimed at increasing transparency, strengthen participatory governance and ensure accountability of our leaders. It is one stop solution for citizens, political parties, government and leaders to establish direct connect with each other, understand the behavior, discuss issues, provide solutions and highlight achievements. ",
+		link: "https://neta.co.in/",
 	},
 	{
-		name: "lorem ipsum",
-		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
-		github: "https://github.com/1hanzla100",
+		name: "Paymentus",
+		desc: "Paymentus was born from a desire to improve the way bills get paid. Vision, innovation and exemplary service have propelled Paymentus to become the leading paperless electronic billing and payment solution on the market, resulting in 1,300 clients including some of the largest billers in North America. ",
+		link: "https://www.paymentus.com/",
 	},
 	{
-		name: "lorem ipsum",
-		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
-		github: "https://github.com/1hanzla100",
-		link: "https://example.com",
+		name: "Outgrow",
+		desc: "Outgrow is a growth marketing platform that enables marketers to build interactive content/tools to increase customer engagement and boost demand generation with no custom design or development.",
+		link: "https://outgrow.co/",
 	},
+	{
+		name: "NDM - Nomis Deal Manager",
+		desc: "NDM is a vertical agnostic presentment and negotiation solution that enables banks to deploy complex pricing/business strategies in a simplified format to front-line representatives while capturing data points (such as salesforce behavior and negotiation history) necessary for banks to make quicker and smarter decisions.",
+		link: "https://www.nomissolutions.com/products/nomis-deal-manager",
+	}
 ];
 
 export const feedbacks = [
 	{
-		name: "Hassan Mehmood",
+		name: "Ajay Rathore",
 		feedback:
-			"We have been very happy with our new website! It looks professional and very easy to navigate. Our experience with the customer service at Hanzla tauqeer has been great. They handle things very efficiently and are available for any questions we have. They also keep us updated on daily reports so we know how our site is doing. I would recommend that u have choose hanzla web developer services for u.",
+			"We have been very happy with our new website! It looks professional and very easy to navigate. Our experience with the customer service at Javaid has been great. He handle things very efficiently and are available for any questions we have. He also keep us updated on daily reports so we know how our site is doing. I would recommend that u have choose Javaid web developer services for u.",
 	},
 	{
-		name: "Maheen Altaf",
+		name: "Tanveera Hassan",
 		feedback:
 			"the website is very nice, everything was perfectly made as i need. it just loads in moments, without giving u the chance to wait. the quality is also very amazing. i am using it without any problem. great job",
 	},
